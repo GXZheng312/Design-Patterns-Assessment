145308cb4962b2e03abed71852abc38c2e051432
JackyZheng657 <j.zheng657@hotmail.com>
2023-06-09 01:12:52 +0200

    WORKS BUT NEEDS TO REFACTOR

--- SudokuSupreme/UI/Draw/SamuraiDraw.cs
+++ SudokuSupreme/UI/Draw/SamuraiDraw.cs
@@ -1,43 +1,176 @@
-﻿using Logic;
+﻿using System;
 
-namespace Presentation.Draw;
-
-public class SamuraiDraw : IDraw
+namespace Presentation.Draw
 {
-    public void Draw(string[] cells)
+    public class SamuraiDraw : IDraw
     {
-        if (cells == null) return;
-
-        Console.WriteLine("Samurai Sudoku Board:");
-
-        for (int i = 0; i < cells.Length; i++)
+        public void Draw(string[] cells)
         {
-            string cellContent = (cells[i] == "0" ? " " : cells[i]);
+            if (cells == null) return;
 
-            int n = i + 1;
+            Console.WriteLine("Samurai Sudoku Board:");
 
-            if (n % 9 == 1)
+            for (int row = 1; row <= 21; row++)
             {
-                Console.Write("|");
-            }
+                if (row == 1)
+                {
+                    Console.WriteLine(HorizontalWall(row));
+                }
+                else if (row <= 9 || row > 12)
+                {
+                    CornerPart(row, cells);
+                }
+                else
+                {
+                    MiddlePart(row, cells);
+                }
 
-            Console.Write($"{cellContent}");
-
-            if (n % 3 == 0)
-            {
-                Console.Write("|");
-            }
-
-            if (n % 9 == 0)
-            {
                 Console.WriteLine();
-            }
 
-            if (n % 27 == 0)
-            {
-                Console.WriteLine("+-----------+");
+                if (row % 3 == 0)
+                {
+                    Console.WriteLine(HorizontalWall(row));
+                }
             }
         }
 
+        private string HorizontalWall(int row)
+        {
+            string wall = "";
+
+            for (int i = 1; i <= 21; i++)
+            {
+                if (i == 1)
+                {
+                    wall += "+";
+                }
+
+                if (i <= 9 || i > 12 || row == 9 || row == 12 || row == 6 || row == 15)
+                {
+                    wall += "-";
+                }
+                else
+                {
+                    wall += " ";
+                }
+
+                if (i % 3 == 0)
+                {
+                    wall += "+";
+                }
+            }
+
+            return wall;
+        }
+
+        private void CornerPart(int row, string[] cells)
+        {
+            for (int col = 1; col <= 21; col++)
+            {
+                if (col == 1)
+                {
+                    Console.Write("|");
+                }
+
+                if (col <= 9 || col > 12)
+                {
+                    int selection = GetCornerSelection(row, col);
+                    string content = cells[selection] == "0" ? " " : cells[selection];
+                    Console.Write(content);
+                }
+                else if (row > 6 && row <= 15)
+                {
+                    int selection = GetMiddleSelection(row, col);
+                    string content = cells[selection] == "0" ? " " : cells[selection];
+                    Console.Write(content);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                if (col % 3 == 0)
+                {
+                    Console.Write("|");
+                }
+            }
+        }
+
+        private int GetCornerSelection(int row, int col)
+        {
+            int selection = 0;
+            int cal = ((row - 1) % 9) * 9;
+
+            if (row > 12)
+            {
+                cal = ((row - (1 + 3)) % 9) * 9;
+                selection += (81 * 3);
+            }
+
+            if (col <= 9)
+            {
+                selection += col - 1;
+            }
+            else
+            {
+                selection += (col - 13) + 81;
+            }
+
+            selection += cal;
+
+            return selection;
+        }
+
+        private int GetMiddleSelection(int row, int col)
+        {
+            int selection = (81 * 2) - 1 + 3;
+            int cal = 0;
+            if (row > 6 && row <= 9)
+            {
+                cal += col - 1 - 9 + ((row - 7) * 9);
+            }
+            else
+            {
+                cal += col - 1 - 9 + ((row - (7 + 6)) * 9) + (9 * 6);
+            }
+
+            selection += cal;
+
+            return selection;
+        }
+
+        private void MiddlePart(int row, string[] cells)
+        {
+            for (int col = 1; col <= 21; col++)
+            {
+                if (col == 1)
+                {
+                    Console.Write(" ");
+                }
+
+                if (col > 6 && col <= 15)
+                {
+                    int selection = GetMiddleSelection(row, col);
+                    string content = cells[selection] == "0" ? " " : cells[selection];
+                    Console.Write(content);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                if (col % 3 == 0)
+                {
+                    if (col >= 6 && col <= 15)
+                    {
+                        Console.Write("|");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+        }
     }
 }
