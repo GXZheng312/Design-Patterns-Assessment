c4e2e697a6f62b0859f9e4731bd551c9361a8315
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-06-16 15:29:03 +0200

    parser
    don't know if it works yet

--- SudokuSupreme/Logic/Parser/SudokuFileParser.cs
+++ SudokuSupreme/Logic/Parser/SudokuFileParser.cs
@@ -22,4 +22,36 @@ public class SudokuFileParser
 
         return numbers;
     }
+
+    public static Dictionary<int, int[]>? ParseJigsawContent(string s)
+    {
+        const int jigsawSize = 81;
+
+        int index = s.IndexOf('=');
+        if (index != -1)
+        {
+            s = s.Substring(index + 1);
+        }
+
+        Dictionary<int, int[]> numbers = new Dictionary<int, int[]>();
+        string[] cells = s.Split('=');
+
+        for (var i = 0; i < cells.Length; i++)
+        {
+            var cell = cells[i];
+            if (cell.Length == 3)
+            {
+                int value = int.Parse(cell.Substring(0, 1));
+                int subIndex = int.Parse(cell.Substring(2, 1));
+                numbers[i] = new [] { value, subIndex };
+            }
+        }
+
+        if (numbers.Count != jigsawSize)
+        {
+            return null;
+        }
+
+        return numbers;
+    }
 }
