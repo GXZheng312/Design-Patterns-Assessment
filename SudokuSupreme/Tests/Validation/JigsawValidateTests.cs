52bd7df5864b9a7e117bc469f3de9385981b20e6
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-06-23 17:11:01 +0200

    Update JigsawValidateTests.cs

--- SudokuSupreme/Tests/Validation/JigsawValidateTests.cs
+++ SudokuSupreme/Tests/Validation/JigsawValidateTests.cs
@@ -9,15 +9,23 @@ public class JigsawValidateTests
     [Test]
     public void ValidJigsawBoard_ShouldReturnTrue()
     {
-        Dictionary<int, List<int[]>> grid = new()
-        {
-            
-        };
+        string celNumbers = "289136745975483126328975461541692387653841972814527693467358219796214538132769854";
+        string grpNumbers = "000000011220033331222333341222355641255556641756666641755776441777784441788888888";
 
         List<Cell> cells = new();
         List<Group> rows = new();
         List<Group> columns = new();
         List<Group> groups = new();
+
+        for (int i = 0; i < celNumbers.Length; i++)
+        {
+            int value = celNumbers[i];
+            int groupNumber = grpNumbers[i];
+            int x = i % 9;
+            int y = i / 9;
+            
+            Cell cell = new Cell(value, x, y);
+        }
 
         JigsawBoard board = new JigsawBoard(cells, groups, rows, columns);
         Assert.True(board.Validate());
